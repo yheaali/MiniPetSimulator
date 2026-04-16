beefa268d09e4822cd80d1541e647dbc8986eed9
yhea <lyhyy379@email.com>
2026-04-17 01:56:17 +0300

    version 1:Add Pet class

---
+++ main.py
@@ -0,0 +1,5 @@
+from pet import Pet
+
+my_pet = Pet("Buddy")
+print(f"Name: {my_pet.name}")
+print(f"Energy: {my_pet.energy_level}")
---
+++ pet.py
@@ -0,0 +1,4 @@
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.energy_level = 50
